eca87b77de28eab2900e87f3329226391505297a
Catalin-code <catalin.despa.code@gmail.com>
2021-02-16 21:47:28 +0200

    Create footer file with dummy row, add bootstrap to package.json

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    <div>
+                        <h4>Column 1</h4>
+                        <ul>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
